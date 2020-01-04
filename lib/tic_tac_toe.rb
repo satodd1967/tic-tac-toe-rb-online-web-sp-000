2abe73ea89f2211fa887842c89f2baa70bb44021
Seth Todd <seth.todd@outlook.com>
2020-01-04 05:12:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,7 +64,7 @@ WIN_COMBINATIONS.detect do |win_combination|
   if ((board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X") ||(board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O")) == nil
     return false
 else
-  
+  (board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X") ||(board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O")
   end
 end
 end
