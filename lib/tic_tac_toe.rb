84328beecd7aa6a412bae0d3242cfe208a56d4b6
Seth Todd <seth.todd@outlook.com>
2020-01-04 05:30:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -61,15 +61,9 @@ end
 
 def won?(board)
 WIN_COMBINATIONS.detect do |win_combination|
-  if ((board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X") ||(board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O")) == false
-    return false
-  else
-    (board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X") ||(board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O")
+  (board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X") || (board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O")
   end
 end
-end
-    
-
 
 def full?(board)
   board.all? do |position|
