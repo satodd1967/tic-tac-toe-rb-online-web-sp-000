175f140e69e2e64f06142be1ea01f1da19406713
Seth Todd <seth.todd@outlook.com>
2020-01-04 02:36:25 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,113 @@
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  converted_input = user_input.to_i - 1
+end
+
+def move(board, input, char)
+  board[input] = char
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end
+
+def valid_move?(board, index)
+  if !index.between?(0, 8)
+    false
+  elsif position_taken?(board, index) == false
+    true
+else
+    false
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index) == false
+    turn(board)
+  end
+    move(board, index, current_player(board))
+    display_board(board)
+  end
+
+def turn_count(board)
+  board.select.count do |count|
+    count == "X" || count == "O"
+  end
+end
+
+def current_player(board)
+  if turn_count(board).even? == true
+    return "X"
+  else
+    "O"
+  end
+end
+
+def won?(board)
+WIN_COMBINATIONS.detect do |win_combination|
+  (board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X") || (board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O")
+  end
+end
+
+def full?(board)
+  board.all? do |position|
+    position == "X" || position == "O"
+  end
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  if won?(board) != nil || draw?(board) == true || full?(board) == true
+    return true
+  else
+    false
+  end
+end
+
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  end
+end
+
+def play(board)
+  turn(board)
+  if over?(board) == true && winner(board) != nil
+    puts "Congratulations #{winner(board)}!"
+  elsif over?(board) == true && winner(board) == nil
+  puts "Cat's Game!"
+    else
+    play(board)
+  end
+end
+
+   
+ 
+
+
+
+
+
+
+  
+
+   
